07a8c57425abf99f25075ea340358e64eb748a9c
zhfan <vanzhang.prc@foxmail.com>
2026-08-20 09:44:44 +0800

    调整获取accesstoken逻辑，避免上锁整个方法

--- src/main/java/van/project/wechat/wechatPublic/services/AccessTokenService.java
+++ src/main/java/van/project/wechat/wechatPublic/services/AccessTokenService.java
@@ -2,32 +2,40 @@ package van.project.wechat.wechatPublic.services;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import van.project.wechat.wechatPublic.common.properties.WechatProperties;
 import van.project.wechat.wechatPublic.services.api.AccessTokenReq;
 import van.project.wechat.wechatPublic.services.api.AccessTokenResp;
 import van.project.wechat.wechatPublic.services.api.WechatFeign;
 
+import java.time.LocalDateTime;
+
 @Slf4j
 @RequiredArgsConstructor
 public class AccessTokenService implements IAccessTokenService {
 
-    protected final WechatProperties wechatProperties;
-    protected final WechatFeign wechatFeign;
+    private final WechatProperties wechatProperties;
+    private final WechatFeign wechatFeign;
 
-    protected volatile String accessToken;
-    protected volatile Long expireAt;
+    private volatile String accessToken;
+    private volatile LocalDateTime expireAt;
+    private final Object lock = new Object();
 
     @Override
-    public synchronized String getAccessToken() {
-        long now = System.currentTimeMillis();
-        if (accessToken == null || now > expireAt) {
-            AccessTokenResp accessTokenResp = wechatFeign.stableToken(new AccessTokenReq(wechatProperties.getAppId(), wechatProperties.getAppSecret()));
-            this.accessToken = accessTokenResp.getAccess_token();
-            long expiresIn = (accessTokenResp.getExpires_in() == null ? 7200L : accessTokenResp.getExpires_in()) - 1800L;
-            this.expireAt = now + expiresIn;
-            log.info("token refreshed, expired in {}", expiresIn);
+    public String getAccessToken() {
+
+        LocalDateTime now = LocalDateTime.now();
+        if (accessToken != null && now.isBefore(expireAt)) {
+            return accessToken;
+        }
+
+        synchronized (lock) {
+            if (accessToken == null || now.isAfter(expireAt)) {
+                AccessTokenResp accessTokenResp = wechatFeign.stableToken(new AccessTokenReq(wechatProperties.getAppId(), wechatProperties.getAppSecret()));
+                this.accessToken = accessTokenResp.getAccess_token();
+                long expiresIn = (accessTokenResp.getExpires_in() == null ? 7200L : accessTokenResp.getExpires_in()) - 1800L;
+                this.expireAt = now.plusSeconds(expiresIn);
+                log.info("token refreshed, expired in {}", expiresIn);
+            }
         }
         return accessToken;
     }
